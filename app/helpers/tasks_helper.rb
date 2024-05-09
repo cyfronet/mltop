3e447613bdd7649ab0cb7fe053e5025647af0e12
Szymon Mazurek <szymonmazurek57@gmail.com>
2024-05-09 19:29:42 +0200

    Table cell coloring based on metric value
    - added helper function to perform mapping a metric value to a color
      from a given range
    - added table cell coloring display in 'tasks' viev to use the defined
      helper function and set proper color

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -15,4 +15,28 @@ module TasksHelper
     def test_set_metric_active?(test_set, metric)
       metric == selected_metric && test_set == selected_test_set
     end
+
+  def interpolate_color(metric)
+
+      value = [0, [100, metric].min].max
+
+      red = [255, 0, 0]
+      yellow = [255, 255, 0]
+      green = [0, 255, 0]
+
+      # Interpolate between red and yellow
+      if value <= 50
+          ratio = value / 50.0
+          r = (red[0] + ratio * (yellow[0] - red[0])).to_i
+          g = (red[1] + ratio * (yellow[1] - red[1])).to_i
+          b = (red[2] + ratio * (yellow[2] - red[2])).to_i
+      # Interpolate between yellow and green
+      else
+          ratio = (value - 50) / 50.0
+          r = (yellow[0] + ratio * (green[0] - yellow[0])).to_i
+          g = (yellow[1] + ratio * (green[1] - yellow[1])).to_i
+          b = (yellow[2] + ratio * (green[2] - yellow[2])).to_i
+      end
+      return "rgb(#{r}, #{g}, #{b})"
+  end
 end
