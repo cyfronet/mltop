979ee6ae48d19aa75d7da1fa2613667af8b14ac6
Marek Kasztelnik <mkasztelnik@gmail.com>
2024-05-10 15:55:03 +0200

    Root page introduction (lorum impsum)

--- test/system/tasks_test.rb
+++ test/system/tasks_test.rb
@@ -3,6 +3,6 @@ require "application_system_test_case"
 class TasksTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit tasks_url
-    assert_selector "h1", text: "Tasks"
+    assert_text "Speech-to-text Translation (ST)"
   end
 end
