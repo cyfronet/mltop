e5f3bb3b56cb36cb52e24308b97796ab8ab1c62e
Marek Kasztelnik <mkasztelnik@gmail.com>
2024-05-15 15:33:29 +0200

    Rename Top::ModelRow to Model::Scores

--- app/models/model/scores.rb
+++ app/models/model/scores.rb
@@ -1,12 +1,9 @@
-class Top::ModelRow
+class Model::Scores
   def initialize(model:, metric:, test_set:)
-    @model = model
-    @metric = metric
     @test_set = test_set
-
     @scores_map = Score.select("value, subtasks.source_language, subtasks.target_language")
       .joins(evaluation: { subtask_test_set: :subtask })
-      .where(metric_id: metric, evaluations: { subtask_test_sets: { test_set_id: test_set } })
+      .where(metric_id: metric, evaluations: { model_id: model, subtask_test_sets: { test_set_id: test_set } })
       .map { |s| [ [ s.source_language, s.target_language ], s.value ] }.to_h
   end
 
--- test/models/model/scores_test.rb
+++ test/models/model/scores_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class Top::RowTest < ActiveSupport::TestCase
+class Model::ScoresTest < ActiveSupport::TestCase
   test "model scores for selected metric and test set" do
-    create(:model, name: "Other task", tasks: [ tasks(:st) ])
+    other_model = create(:model, name: "Other task", tasks: [ tasks(:st) ])
     model = create(:model, name: "Model", tasks: [ tasks(:st) ])
 
     evaluation = create(:evaluation, model:,
@@ -17,12 +17,18 @@ class Top::RowTest < ActiveSupport::TestCase
     create(:score, evaluation:, metric: metrics("blue"), value: 3)
     create(:score, evaluation:, metric: metrics("chrf"), value: 4)
 
-    scores = Top::ModelRow.new(model:, test_set: test_sets("flores"), metric: metrics("blue"))
+    evaluation = create(:evaluation, model: other_model,
+                        evaluator: evaluators(:sacrebley),
+                        subtask_test_set: subtask_test_sets(:flores_en_pl))
+    create(:score, evaluation:, metric: metrics("blue"), value: 5)
+    create(:score, evaluation:, metric: metrics("chrf"), value: 6)
+
+    scores = Model::Scores.new(model:, test_set: test_sets("flores"), metric: metrics("blue"))
     assert_nil scores.score("en", "pt"), "Nil should be returned for non existing score"
     assert_equal 1, scores.score("en", "pl")
     assert_equal 3, scores.score("en", "it")
 
-    scores = Top::ModelRow.new(model:, test_set: test_sets("flores"), metric: metrics("chrf"))
+    scores = Model::Scores.new(model:, test_set: test_sets("flores"), metric: metrics("chrf"))
     assert_nil scores.score("en", "pt"), "Nil should be returned for non existing score"
     assert_equal 2, scores.score("en", "pl")
     assert_equal 4, scores.score("en", "it")
