b56975b27a08362d26444a3486e3939adaff5d77
Marek Kasztelnik <mkasztelnik@gmail.com>
2024-07-05 11:18:08 +0200

    Add test sets index page

--- app/controllers/test_sets_controller.rb
+++ app/controllers/test_sets_controller.rb
@@ -1,5 +1,9 @@
 class TestSetsController < ApplicationController
-  allow_unauthenticated_access only: [ :show ]
+  allow_unauthenticated_access
+
+  def index
+    @test_sets = TestSet.includes(:tasks).all
+  end
 
   def show
     @test_set = TestSet.find(params[:id])
--- test/controllers/test_sets_controller_test.rb
+++ test/controllers/test_sets_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class TestSetsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get test_sets_path
+    assert_response :success
+  end
+
   test "should get show" do
     get test_set_path(test_sets(:flores))
     assert_response :success
