763c4842860aa0f0439a98290aaa0b075043e508
Piotr Połeć <piotr.jan.polec@gmail.com>
2024-07-19 11:21:06 +0200

    add groundtruth language validation

--- app/models/groundtruth.rb
+++ app/models/groundtruth.rb
@@ -1,12 +1,14 @@
 class Groundtruth < ApplicationRecord
+  LANGUAGES = %w[be bg bs ca cs da de el en en es et fi fr ga gl hr hu is it lb lt lv mk mt nl no pl pr pt ro ru sk sl sr sv th tr uk]
+
   belongs_to :test_set_entry
   belongs_to :task
   has_many :hypotheses, dependent: :destroy
 
   has_one_attached :input
 
-  validates :language, presence: true, uniqueness: { scope: :test_set_entry }
-
+  validates :language, presence: true, uniqueness: { scope: :test_set_entry },
+  inclusion: { in: LANGUAGES, message: "%{value} is not a correct language code" }
 
   def to_s
     "#{test_set_entry.language}->#{language}"
