6911733f538bab2af7369efee4236e7522b245dd
Piotr Połeć <piotr.jan.polec@gmail.com>
2024-07-19 10:59:31 +0200

    use grountdruths in top row and rows

--- app/models/top/row.rb
+++ app/models/top/row.rb
@@ -5,46 +5,46 @@ class Top::Row
 
   delegate :name, to: :model
 
-  def initialize(model, scores, subtasks_count)
+  def initialize(model, scores, groundtruths_count)
     @model = model
     @scores = scores
-    @subtasks_count = subtasks_count
+    @groundtruths_count = groundtruths_count
     @cached_scores = {}
   end
 
   def self.where(task:, test_set: nil)
     scores = Score
-      .joins(evaluation: { hypothesis: { groundtruth: [ :subtask, :test_set_entry ] } })
-      .where(evaluation: { hypotheses: { groundtruths: { subtasks: { task_id: task } } } })
+      .joins(evaluation: { hypothesis: { groundtruth: [ :test_set_entry ] } })
+      .where(evaluation: { hypotheses: { groundtruths: { task_id: task } } })
     scores = scores.where(evaluation: { hypotheses: { groundtruths: { test_set_entries: { test_set_id: test_set } } } }) if test_set
 
-    scores = scores.select("scores.value, scores.metric_id, hypotheses.model_id, groundtruths.subtask_id, test_set_entries.test_set_id")
-    subtasks_count = task.subtasks.size
+    scores = scores.select("scores.value, scores.metric_id, hypotheses.model_id, test_set_entries.test_set_id, groundtruths.id as groundtruth_id")
+    groundtruths_count = task.groundtruths.size
 
     scores_by_model_id = scores.group_by { |score| score.model_id }
     models = Model.where(id: scores_by_model_id.keys).index_by(&:id)
-    rows = scores_by_model_id.map { |model_id, scores| new(models[model_id], scores, subtasks_count) }
+    rows = scores_by_model_id.map { |model_id, scores| new(models[model_id], scores, groundtruths_count) }
 
     Top::Rows.new(rows)
   end
 
-  def score(test_set:, metric:, subtask: nil)
-    @cached_scores[[ test_set, metric, subtask ]] ||= calculate_score(test_set:, metric:, subtask:)
+  def score(test_set:, metric:, groundtruth: nil)
+    @cached_scores[[ test_set, metric, groundtruth ]] ||= calculate_score(test_set:, metric:, groundtruth:)
   end
 
-  def calculate_score(test_set:, metric:, subtask: nil)
+  def calculate_score(test_set:, metric:, groundtruth: nil)
     scores = @scores.filter do |score|
       (!test_set || score.test_set_id == test_set.id) &&
         (!metric || score.metric_id == metric.id)
     end
 
-    scores = if subtask
-      scores.filter { |score| score.subtask_id == subtask.id }
+    scores = if groundtruth
+      scores.filter { |score| score.groundtruth_id == groundtruth.id }
     else
       scores
         .group_by { |score| [ score.metric_id, score.test_set_id ] }
         .values.map do |list|
-          Score.new(metric:, value: list.sum(&:value) / @subtasks_count)
+          Score.new(metric:, value: list.sum(&:value) / @groundtruths_count)
         end
     end
 
