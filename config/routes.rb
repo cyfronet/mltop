eec65eeedfcb86c9004f14865109501c23ba6c40
Piotr Połeć <piotr.jan.polec@gmail.com>
2024-07-24 14:18:35 +0200

    add task test set controller

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
     resources :tasks do
       resources :entries, only: :index, module: :tasks
       resources :groundtruths, only: [ :new, :create ], module: :tasks
+      resources :test_sets, only: [ :new, :create ], module: :tasks
     end
     resources :test_sets do
       resources :entries, only: :index, module: :test_sets
