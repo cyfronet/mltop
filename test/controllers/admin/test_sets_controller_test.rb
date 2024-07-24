ca03da46b7dd1e033650376d3afa361966269e4b
Szymon Mazurek <szymonmazurek57@gmail.com>
2024-07-24 23:02:18 +0200

    Implementing remaining tests for the test_sets controller

--- test/controllers/admin/test_sets_controller_test.rb
+++ test/controllers/admin/test_sets_controller_test.rb
@@ -19,5 +19,29 @@ class Admin::TestSetsControllerTest < ActionDispatch::IntegrationTest
       post admin_test_sets_url, params: { test_set: { name: test_set.name } }
     end
     assert_redirected_to admin_test_set_path(TestSet.last)
+  end
+  test "should show test set" do
+    test_set = create(:test_set)
+
+    get admin_test_set_path(test_set)
+    assert_response :success
+  end
+  test "should get edit" do
+    test_set = create(:test_set)
+    get edit_admin_test_set_path(test_set)
+    assert_response :success
+  end
+  test "should update user" do
+    test_set = create(:test_set)
+    patch admin_test_set_path(test_set), params: { test_set: { name: "updated name" } }
+    assert_redirected_to admin_test_set_path(test_set)
+  end
+
+  test "should destroy user" do
+    test_set = create(:test_set)
+    assert_difference("TestSet.count", -1) do
+      delete admin_test_set_path(test_set)
     end
+    assert_redirected_to admin_test_sets_path
+  end
 end
