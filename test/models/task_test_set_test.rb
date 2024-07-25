bf120aa5c81c807f0713ed7c0b165a7f306ea5fb
Piotr Połeć <piotr.jan.polec@gmail.com>
2024-07-25 09:01:24 +0200

    add tests

--- test/models/task_test_set_test.rb
+++ test/models/task_test_set_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class TaskTestSetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is unable to create a duplicate task test set" do
+    task_test_set = TaskTestSet.new(
+      task: TaskTestSet.first.task,
+      test_set: TaskTestSet.first.test_set
+    )
+    assert_equal task_test_set.valid?, false
+  end
 end
