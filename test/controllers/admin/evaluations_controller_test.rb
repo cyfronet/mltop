7bcb1f116c603d2bf2473f2f3b86897c821cfa9d
Marek Kasztelnik <mkasztelnik@gmail.com>
2024-07-31 14:29:31 +0200

    Simple crud for evaluators

---
+++ test/controllers/admin/evaluations_controller_test.rb
@@ -0,0 +1,59 @@
+require "test_helper"
+
+class Admin::EvaluatorsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in_as("marek")
+  end
+
+  test "should get index" do
+    get admin_evaluators_path
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_admin_evaluator_path
+    assert_response :success
+  end
+
+  test "should create new evaluator" do
+    assert_difference("Evaluator.count") do
+      post admin_evaluators_url, params: { evaluator: {
+        name: "New evaluator name"
+      } }
+    end
+
+    assert_redirected_to admin_evaluator_path(Evaluator.last)
+  end
+
+  test "should show evaluator" do
+    evaluator = evaluators(:sacrebley)
+
+    get admin_evaluator_path(evaluator)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    evaluator = evaluators(:sacrebley)
+
+    get edit_admin_evaluator_path(evaluator)
+    assert_response :success
+  end
+
+  test "should update evaluator" do
+    evaluator = evaluators(:sacrebley)
+
+    patch admin_evaluator_path(evaluator), params: { evaluator: { name: "updated name" } }
+    assert_redirected_to admin_evaluator_path(evaluator)
+    assert_equal "updated name", evaluator.reload.name
+  end
+
+  test "should destroy evaluator" do
+    evaluator = evaluators(:sacrebley)
+
+    assert_difference("Evaluator.count", -1) do
+      delete admin_evaluator_path(evaluator)
+    end
+
+    assert_redirected_to admin_evaluators_path
+  end
+end
