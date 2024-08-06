ebe7a9d337b9f497f8cbf532da71f3671682e98e
Piotr Połeć <piotr.jan.polec@gmail.com>
2024-08-06 15:20:07 +0200

    add hypotheses controller

---
+++ app/controllers/submissions/hypotheses_controller.rb
@@ -0,0 +1,23 @@
+class Submissions::HypothesesController < ApplicationController
+  def new
+    @model = Model.find(params[:submission_id])
+    @groundtruth = Groundtruth.find(params[:groundtruth_id])
+    @hypothesis = Hypothesis.new(model: @model, groundtruth: @groundtruth)
+  end
+
+  def create
+    @hypothesis = Hypothesis.new(hypothesis_params)
+    @groundtruth = @hypothesis.groundtruth
+    if @hypothesis.save
+      flash.now[:notice] = "Hypothesis succesfully created"
+    else
+      @groundtruth = @hypothesis.groundtruth
+      render(:new, status: :unprocessable_entity)
+    end
+  end
+
+  private
+  def hypothesis_params
+    params.require(:hypothesis).permit(:model_id, :groundtruth_id, :input)
+  end
+end
