9c4dfe705b76d5cb716ea4b7a093736aea630f2f
Marek Kasztelnik <mkasztelnik@gmail.com>
2024-12-05 17:53:23 +0100

    Add/remove meetween_member role basing on plggmeetween group
    Meetween research team members are receiving `meetween_member` role.
    When user is removed from the group this role is removed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   encrypts :ssh_key, :ssh_certificate
   has_many :models, inverse_of: :owner, dependent: :destroy
 
-  roles :admin
+  roles :admin, :meetween_member
 
   def credentials_valid?
     ssh_credentials.valid?
