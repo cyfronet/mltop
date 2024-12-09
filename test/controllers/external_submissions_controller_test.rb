31539e7d4590445e0015cdec61b66c66a0437102
Marek Kasztelnik <mkasztelnik@gmail.com>
2024-12-09 11:07:21 +0100

    Only Meetween members can see external users submissions

--- test/controllers/external_submissions_controller_test.rb
+++ test/controllers/external_submissions_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class ExternalSubmissionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Meetween member can manage external users submissions" do
+    sign_in_as("marek")
+
+    get external_submissions_path
+    assert_response :success
+  end
+
+  test "External user cannot manage external users submissions" do
+    sign_in_as("external", teams: [ "plggother" ])
+
+    get external_submissions_path
+    assert_response :redirect
+
+    follow_redirect!
+    assert_includes response.body, "Only Meetween members can manage"
+  end
 end
