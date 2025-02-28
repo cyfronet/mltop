3264f461c86e8b316de7a5efdfac3799c48e6c09
Piotr Połeć <piotr.jan.polec@gmail.com>
2025-02-28 12:44:13 +0100

    validate task evaluator, tests

--- app/models/task_evaluator.rb
+++ app/models/task_evaluator.rb
@@ -3,4 +3,15 @@ class TaskEvaluator < ApplicationRecord
   belongs_to :evaluator
 
   validates :evaluator, uniqueness: { scope: :task_id }
+  validate :matching_task_evaluator
+
+  def matching_task_evaluator
+    if evaluator.input_modality && evaluator.input_modality != task.from
+      errors.add(:evaluator, "Input modality of evaluator does not match task")
+    end
+
+    if evaluator.output_modality && evaluator.output_modality != task.to
+      errors.add(:evaluator, "Output modality of evaluator does not match task")
+    end
+  end
 end
--- test/models/evaluator_test.rb
+++ test/models/evaluator_test.rb
@@ -1,4 +1,16 @@
 require "test_helper"
 
 class EvaluatorTest < ActiveSupport::TestCase
+  test ".mathcing_task" do
+    task = tasks(:st) # audio, text
+
+    create(:evaluator, input_modality: "audio", output_modality: "video")
+    create(:evaluator, input_modality: "audio", output_modality: nil)
+    matching_evaluators = [
+      create(:evaluator, input_modality: "audio", output_modality: "text"),
+      create(:evaluator, input_modality: nil, output_modality: "text")
+    ]
+
+    assert_equal matching_evaluators, Evaluator.matching_task(task)
+  end
 end
