727a6cb7b7bd195becb72cc0acc18253fb59125d
Marek Kasztelnik <mkasztelnik@gmail.com>
2025-03-13 12:16:52 +0100

    Preloading task test sets does not filter tasks

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -6,4 +6,11 @@ class TaskTest < ActiveSupport::TestCase
 
     assert_equal test_sets(:flores, :mustc), st.test_sets.to_a
   end
+
+  test "#with_published_test_sets does not filter tasks" do
+    mt = Task.with_published_test_sets.find(tasks(:mt).id)
+
+    assert mt
+    assert_equal [], mt.test_sets.to_a
+  end
 end
