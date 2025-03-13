110b345fb4ba8b6f7851e6be39c25a6e2153dadd
Marek Kasztelnik <mkasztelnik@gmail.com>
2025-03-13 12:31:29 +0100

    IWSLT 2025 environment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-if Rails.env.production? && ENV["SENTRY_DSN"]
+if !Rails.env.local? && ENV["SENTRY_DSN"]
   Sentry.init do |config|
     config.dsn = ENV["SENTRY_DSN"]
     config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
 
-    config.enabled_environments = %w[production staging]
+    config.enabled_environments = %w[production staging iwslt]
 
     config.traces_sample_rate = 0.5
 
