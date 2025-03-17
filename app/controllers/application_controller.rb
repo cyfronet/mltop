1572b33b310ebb08c0e38dd9e57eb5608b4d355b
Marek Kasztelnik <mkasztelnik@gmail.com>
2025-03-17 10:36:41 +0100

    Add missing render_404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   include Sentryable
 
   allow_browser versions: :modern
+
+  def render_404
+    render file: "#{Rails.root}/public/404", status: :not_found
+  end
 end
