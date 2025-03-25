82198e0b761b41f2957e203f3efdf7dc78a87db8
Piotr Połeć <piotr.jan.polec@gmail.com>
2025-03-25 14:28:24 +0100

    add data consent to model, fix policy for submission

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,2 +1,9 @@
 module SubmissionsHelper
+  def data_consent_description
+    """
+      Do you agree that the IWSLT organizers may release your submitted system
+      output data under an Apache 2.0 or similar license (depending on licensing details of test sets),
+      to encourage future research? We may exclude your submissions from human evaluation without this consent.
+    """
+  end
 end
