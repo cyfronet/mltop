63cd44293d7be2736104a0ede9e5c97524d4fdee
Piotr Połeć <piotr.jan.polec@gmail.com>
2025-03-31 13:14:15 +0200

    add reference to challenge in task

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,7 +2,7 @@ class Challenge < ApplicationRecord
   belongs_to :owner, required: true, class_name: "User"
 
   has_many :models, dependent: :destroy
-  has_many :tasks, dependent: :destroy
+  has_many :tasks, dependent: :nullify
 
   has_rich_text :description
 
