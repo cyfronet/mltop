b535a7a574e81a4b98e6ed16ff56f69601ff1cf9
Piotr Połeć <piotr.jan.polec@gmail.com>
2025-04-25 13:44:21 +0200

    handle nils in Top::Rows.order

--- app/models/top/rows.rb
+++ app/models/top/rows.rb
@@ -8,11 +8,18 @@ class Top::Rows
   def order(test_set:, metric:, order: "desc", test_set_entry: nil)
     if test_set && metric
       rows = @rows.sort do |a, b|
-        a, b = b, a if order == "desc"
-        a, b = b, a if metric.asc?
+        a_val = a.score(test_set:, metric:, test_set_entry:).value
+        b_val = b.score(test_set:, metric:, test_set_entry:).value
 
-        a.score(test_set:, metric:, test_set_entry:).value <=>
-          b.score(test_set:, metric:, test_set_entry:).value
+        next 1 if a_val.nil?
+        next -1 if b_val.nil?
+
+        # Reverse with XOR if we want descending OR the metric prefers lower scores
+        if (order == "desc") ^ metric.asc?
+          b_val <=> a_val
+        else
+          a_val <=> b_val
+        end
       end
 
       Top::Rows.new(rows)
