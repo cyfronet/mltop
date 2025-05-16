a31514f1176e9a85a97522b7552b68d1b60b1071
Piotr Połeć <piotr.jan.polec@gmail.com>
2025-05-16 15:14:29 +0200

    add score value validation

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,4 +4,14 @@ class Score < ApplicationRecord
 
   validates :value, presence: true, numericality: true
   validates :metric, uniqueness: { scope: :evaluation_id }
+  validate :value_within_range
+
+  def value_within_range
+    proper_range = if metric.asc?
+      (metric.best_score..metric.worst_score).cover?(value)
+    else
+      (metric.worst_score..metric.best_score).cover?(value)
+    end
+    errors.add(:value, "is not within the range of the metric") unless proper_range
+  end
 end
