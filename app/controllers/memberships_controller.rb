39399630cf95ef35b8a20562da6a4a5e3381f00b
Piotr Połeć <piotr.jan.polec@gmail.com>
2025-06-03 14:22:26 +0200

    redirect when user is already member of a challenge

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,5 +1,10 @@
 class MembershipsController < ApplicationController
   def create
+    if Current.challenge_member?
+      redirect_back fallback_location: root_path, alert: "Already member of this challenge."
+      return
+    end
+
     if Membership.create(user: Current.user, challenge: Current.challenge)
       redirect_to root_path, notice: "Successfully joined challenge."
     else
