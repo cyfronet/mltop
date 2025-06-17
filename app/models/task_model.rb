da2590b07249b9ca15800404002196f8565d3e7e
Piotr Połeć <piotr.jan.polec@gmail.com>
2025-06-17 15:53:57 +0200

    add challenge_id validation when creating TaskModel

--- app/models/task_model.rb
+++ app/models/task_model.rb
@@ -3,4 +3,9 @@ class TaskModel < ApplicationRecord
   belongs_to :model
 
   validates :model, uniqueness: { scope: :task_id }
+  validate :challenge_alignment
+
+  def challenge_alignment
+    errors.add(:model_id, :invalid) if model.challenge_id != task.challenge_id
+  end
 end
