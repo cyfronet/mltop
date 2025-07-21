020eb81145d7a7c5c7656b7873b2513d539c48bb
Piotr Połeć <piotr.jan.polec@gmail.com>
2025-07-21 15:16:49 +0200

    create external submission policy

--- app/controllers/challenges/dashboard/external_submissions_controller.rb
+++ app/controllers/challenges/dashboard/external_submissions_controller.rb
@@ -2,8 +2,8 @@ module Challenges
   module Dashboard
     class ExternalSubmissionsController < ApplicationController
       def index
-        authorize(Model)
-        @models = policy_scope(Model).external.with_not_evaluated_hypothesis
+        authorize(Model, policy_class: ExternalSubmissionPolicy)
+        @models = policy_scope(:external_submission).external.with_not_evaluated_hypothesis
       end
     end
   end
