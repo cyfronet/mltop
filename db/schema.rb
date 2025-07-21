055c6a2354f44819b4f8c76b7d92017933f773d2
Piotr Połeć <piotr.jan.polec@gmail.com>
2025-07-21 15:25:00 +0200

    add visibility to challenge

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_04_124445) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_21_102623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
+  create_enum "challenge_visibility", ["leaderboard_released", "scores_released"]
   create_enum "consent_target", ["model", "challenge"]
   create_enum "format", ["video", "audio", "text"]
 
@@ -75,6 +76,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_04_124445) do
     t.datetime "ends_at", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.enum "visibility", enum_type: "challenge_visibility"
     t.index ["owner_id"], name: "index_challenges_on_owner_id"
   end
 
