55a798740c467e15cedcbd2e3fb68befca3fc38e
Piotr Połeć <piotr.jan.polec@gmail.com>
2025-07-30 11:30:47 +0200

    tests

--- test/controllers/challenges/submissions/evaluations_controller_test.rb
+++ test/controllers/challenges/submissions/evaluations_controller_test.rb
@@ -6,48 +6,53 @@ module Challenges
       include ActiveJob::TestHelper
 
       def setup
-        in_challenge!
       end
 
-      test "Meetween members can run owned model evaluations" do
+      test "Challenge managers can run owned model evaluations" do
         model = create(:model)
         hypothesis = create(:hypothesis, model:)
         Hypothesis.any_instance.stubs(:evaluate!)
 
         sign_in_as("marek")
+        in_challenge!(users(:marek), "manager")
         post hypothesis_evaluations_path(hypothesis_id: hypothesis, format: :turbo_stream)
 
         assert_response :success
         assert_equal "Evaluations queued to submit", flash[:notice]
       end
 
-      test "Not Meetween member cannot start owned model evaluation" do
+      test "Participant cannot start owned model evaluation" do
         model = create(:model, owner: users("external"))
         hypothesis = create(:hypothesis, model:)
         create(:evaluation, hypothesis:)
 
-        sign_in_as("external", teams: [ "plgother" ])
+        sign_in_as("external", teams: [ "plggother" ])
+        in_challenge!(users(:external), "participant")
         post hypothesis_evaluations_path(hypothesis_id: hypothesis, format: :turbo_stream)
 
         assert_response :redirect
         assert_equal "You are not authorized to perform this action", flash[:alert]
       end
 
-      test "Meetween members cannot start other meetween user model evaluation" do
+      test "Challenge managers cannot start other managers model evaluation" do
         model =  create(:model, owner: users("szymon"))
+        users(:szymon).update(groups: [ "plggmeetween" ])
+        create(:membership, user: users(:szymon), challenge: challenges(:global))
         hypothesis = create(:hypothesis, model:)
 
         sign_in_as("marek")
+        in_challenge!(users(:marek), "manager")
         post hypothesis_evaluations_path(hypothesis_id: hypothesis, format: :turbo_stream)
 
         assert_response :not_found
       end
 
-      test "Meetween members can start external users model evaluations" do
+      test "Challenge managers can start external users model evaluations" do
         model =  create(:model, owner: users("external"))
         hypothesis = create(:hypothesis, model:)
 
         sign_in_as("marek")
+        in_challenge!(users(:marek), "manager")
         post hypothesis_evaluations_path(hypothesis_id: hypothesis, format: :turbo_stream)
 
         assert_response :success
--- test/integration/sessions_test.rb
+++ test/integration/sessions_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class SessionsTest < ActionDispatch::IntegrationTest
-  test "meetween user can log in through PLGrid and see external users submissions" do
-    in_challenge!
+  test "Challenge manager can log in through PLGrid and see external users submissions" do
+    in_challenge!(users(:marek), :manager)
     sign_in_as("marek", teams: [ "plggmeetween", "plgother" ])
     get root_path
 
@@ -11,7 +11,7 @@ class SessionsTest < ActionDispatch::IntegrationTest
     assert_match "External submissions", response.body
   end
 
-  test "other plgrid users can log in through PLGrid, but do not see external submissions" do
+  test "Non challenge managers can log in through PLGrid, but do not see external submissions" do
     sign_in_as("marek", teams: [ "plggother" ])
     in_challenge!
 
