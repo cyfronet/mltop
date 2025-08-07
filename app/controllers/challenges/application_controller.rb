34b673f23d3e50180dfd1f57f345c85c7cd0994c
Marek Kasztelnik <mkasztelnik@gmail.com>
2025-08-07 13:15:30 +0200

    Scopes authorization

--- app/controllers/challenges/application_controller.rb
+++ app/controllers/challenges/application_controller.rb
@@ -1,15 +1,5 @@
 module Challenges
   class ApplicationController < ApplicationController
-    def policy_scope(scope, **args)
-      super([ :challenges, scope ], **args)
-    end
-
-    def authorize(record, **args)
-      super([ :challenges, record ], **args)
-    end
-
-    def permitted_attributes(record, **args)
-      super([ :challenges, record ], **args)
-    end
+    scoped_authentication :challenges
   end
 end
--- app/controllers/challenges/public/application_controller.rb
+++ app/controllers/challenges/public/application_controller.rb
@@ -2,18 +2,7 @@ module Challenges
   module Public
     class ApplicationController < ApplicationController
       allow_unauthenticated_access
-
-      def policy_scope(scope, **args)
-        super([ :challenges, :public, scope ], **args)
-      end
-
-      def authorize(record, query, **args)
-        super([ :challenges, :public, record ], query, **args)
-      end
-
-      def permitted_attributes(record, **args)
-        super([ :challenges, :public, record ], **args)
-      end
+      scoped_authentication :challenges, :public
     end
   end
 end
