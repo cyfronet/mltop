5d4d00602d6175a26b13c747624fa17598ccd869
Piotr Połeć <piotr.jan.polec@gmail.com>
2025-08-07 11:58:04 +0200

    move public things to public domain, refactor policies

---
+++ app/controllers/challenges/public/models_controller.rb
@@ -0,0 +1,31 @@
+module Challenges
+  module Public
+    class ModelsController < ApplicationController
+      def index
+        @models = policy_scope(Model)
+      end
+
+      def show
+        @model =  policy_scope(Model).includes(:tasks).find(params[:id])
+
+        @tasks = @model.tasks
+        @task = find_by_query_param(@tasks, :tid)
+
+        @test_sets = @task.test_sets.published
+        @metrics = @task.metrics
+
+        @test_set = find_by_query_param(@test_sets, :tsid)
+        @metric = find_by_query_param(@metrics, :mid)
+        @model_row = Model::Scores.new(model: @model, task: @task, metric: @metric, test_set: @test_set)
+      end
+
+      private
+        def find_by_query_param(collection, key)
+          collection = collection.to_a
+
+          collection.to_a.find { |record| record.id.to_s == params[key] } ||
+          collection.first
+        end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,19 +9,21 @@ Rails.application.routes.draw do
 
   constraints(-> { _1.env["mltop.challenge_id"] }) do
     scope module: :challenges do
-      root to: "tasks#index", as: :challenge_root
+      root to: "public/tasks#index", as: :challenge_root
 
       resources :evaluators, only: [ :index ]
       resource :membership, only: [ :new, :create ]
 
-      resources :tasks do
-        resource :leaderboard, only: :show, module: :tasks
+      scope module: :public do
+        resources :tasks do
+          resource :leaderboard, only: :show, module: :tasks
+        end
+        resources :test_sets, only: [ :show, :index ] do
+          resource :leaderboard, only: :show, module: :test_sets
+        end
+        resources :test_set, only: [ :show ]
+        resources :models, only: [ :index, :show ]
       end
-      resources :test_sets, only: [ :show, :index ] do
-        resource :leaderboard, only: :show, module: :test_sets
-      end
-      resources :test_set, only: [ :show ]
-      resources :models, only: [ :index, :show ]
 
       resources :submissions do
         resources :tasks, only: [ :index, :show ], module: :submissions
