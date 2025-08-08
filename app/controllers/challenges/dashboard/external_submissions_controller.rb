08d2272146c26949ee93342912ff9bc4fd5b459b
Marek Kasztelnik <mkasztelnik@gmail.com>
2025-08-08 14:27:27 +0200

    Fix method name

--- app/controllers/challenges/dashboard/external_submissions_controller.rb
+++ app/controllers/challenges/dashboard/external_submissions_controller.rb
@@ -3,7 +3,7 @@ module Challenges
     class ExternalSubmissionsController < ApplicationController
       def index
         authorize(Model)
-        @models = policy_scope(Model).external.with_not_evaluated_hypothesis
+        @models = policy_scope(Model).external.with_not_evaluated_hypotheses
       end
     end
   end
