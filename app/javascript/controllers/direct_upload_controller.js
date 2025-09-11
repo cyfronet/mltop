da559f2290974544a944b82837e094437cd8426e
Piotr Połeć <piotr.jan.polec@gmail.com>
2025-09-11 12:22:21 +0200

    add direct upload controller

---
+++ app/javascript/controllers/direct_upload_controller.js
@@ -0,0 +1,61 @@
+import { Controller } from "@hotwired/stimulus"
+import { DirectUpload } from "@rails/activestorage"
+
+export default class extends Controller {
+  static targets = ["input", "text"]
+
+  connect() {
+    this.originalText = this.textTarget.innerHTML
+  }
+
+  upload() {
+    this.textTarget.innerHTML = "Uploading..."
+    Array.from(this.inputTarget.files).forEach(file => {
+      this.uploadFile(file)
+    })
+    this.inputTarget.value = null
+    this.element.closest('form').submit()
+  }
+
+  dragenter(event) {
+    event.preventDefault()
+    this.textTarget.innerHTML = "Drop to upload"
+  }
+
+  dragleave(event) {
+    event.preventDefault()
+    this.textTarget.innerHTML = this.originalText
+  }
+
+  drop(event) {
+    event.preventDefault()
+    const files = event.dataTransfer.files
+    this.textTarget.innerHTML = "Uploading..."
+    Array.from(files).forEach(file => {
+      this.uploadFile(file)
+    })
+    this.element.closest('form').submit()
+  }
+
+  dragover(event) {
+    event.preventDefault()
+  }
+
+  uploadFile(file) {
+    const url = this.inputTarget.dataset.directUploadUrl
+    const upload = new DirectUpload(file, url)
+
+    upload.create((error, blob) => {
+      if (error) {
+        // Handle the error
+        console.error("Direct upload failed:", error)
+      } else {
+        const hiddenField = document.createElement('input')
+        hiddenField.setAttribute("type", "hidden")
+        hiddenField.setAttribute("value", blob.signed_id)
+        hiddenField.name = this.inputTarget.name
+        this.element.closest('form').appendChild(hiddenField)
+      }
+    })
+  }
+}
