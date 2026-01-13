742999d659266c4c314db243aaa6f604790d2ed9
Piotr Połeć <piotr.jan.polec@gmail.com>
2026-01-13 12:33:46 +0100

    extract prosopite to concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,10 @@ class ApplicationController < ActionController::Base
   include Authentication
   include Authorization
   include Sentryable
-
+  include SqlQueryProfiling
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
-
-  if Rails.env.development?
-    around_action :n_plus_one_detection
-
-    def n_plus_one_detection
-      Prosopite.scan
-      yield
-    ensure
-      Prosopite.finish
-    end
-  end
 end
